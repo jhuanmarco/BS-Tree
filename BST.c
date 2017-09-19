89a348a0cde81ad9f8f9917bef3ffdef044aabc0
jhuanmarco <jhuanmarco@gmail.com>
2017-09-19 15:49:23 -0300

    first commit

---
+++ BST.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int info;
+	struct node *left;
+	struct node *right;
+} NODE;
+
+
+char mainMenu(){
+	char option;
+	printf("\nBinary Search Tree (BST)\n\nType:\n1 - Create BST\n2 - Add Node\n3 - Del Node\n4 - Print BST\n0 - Exit\n");
+	scanf(" %c", &option);
+	printf("\n");
+	
+	return option;
+}
+
+int returnInfo(){
+	int info;
+	printf("Type Node Info: ");
+	scanf(" %d", &info);
+	
+	return info; 
+}
+
+NODE *createNode(){
+	NODE *newNode = malloc(sizeof(NODE));
+	if(!newNode){
+		 printf("Error: No Memory");
+		 return NULL;
+	}
+	
+	newNode->info = returnInfo();
+	newNode->left = NULL;
+	newNode->right = NULL;
+	printf("Node Created, Info = %d\n", newNode->info);
+	
+	return newNode;
+}
+
+void searchAddNode(NODE *node, NODE *new, int info){
+	
+	if(node->info < info){
+		if(node->left){
+			node = node->left;
+			searchAddNode(node, new, info);
+		}
+			node->left = new;
+	} else {
+		if(node->right){
+			node = node->right;
+			searchAddNode(node, new, info);
+		}
+			node->right = new;
+	}
+	
+	
+	return;
+}
+
+void addNode(NODE *root){
+	NODE *newNode = createNode();
+	
+	if(!newNode){
+		 printf("Error: No Memory");
+		 return;
+	}
+	
+	newNode->left = NULL;
+	newNode->right = NULL;
+	
+	searchAddNode(root, newNode, newNode->info);
+
+}
+
+
+
+
+//PRINT
+
+
+void erd (NODE *r) {
+   if (r != NULL) {
+      erd (r->left);
+      printf ("%d\n", r->info);
+      erd (r->right); 
+   }
+}
+
+
+void main(){
+	char menu;
+	NODE *root = NULL;
+		
+	
+	do {
+		menu = mainMenu();
+		switch(menu){
+			case '1':
+				if(root){
+					printf("BST Root Already Created\n\n");
+					break;
+				}
+				
+				root = createNode();
+				
+			break;
+			case '2':
+				if(!root){
+					printf("BST Not Created, Go to Opt.1\n\n");
+					break;
+				}
+				
+				addNode(root);
+			
+			break;
+			case '3':
+			
+			break;
+			case '4':
+				erd(root);
+			
+			break;
+			default:
+			
+			break;
+		}
+		
+		
+
+	} while(menu != '0');
+}	
